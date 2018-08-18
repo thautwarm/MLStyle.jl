ebad1a3bd60db400566ce9edd6963094665a3660
thautwarm <twshere@outlook.com>
2018-08-18 18:23:44 +0800

    fix version compatibility problem in tests

--- test/typelevel.jl
+++ test/typelevel.jl
@@ -9,17 +9,23 @@
     @testset "type level for func pattern" begin
         f = (Int ⇒ Int)(x -> x)
         Feature.@activate TypeLevel
-        @test @match f begin
-            ::(T ⇒ G)  => T == G
-            _          => false
-        end
+        function()
+                 @match f begin
+                ::(T ⇒ G)  => T == G
+                _          => false
+                end
+        end |> it -> @test it()
+
     end
 
     @testset "type level for type parameter destruction" begin
         @case S{A, B}(a :: A, b :: B)
         s = S(1, "2")
-        @test @match s begin
-            ::Kind{A, B} => Kind === S && A === Int && B === String
-        end
+        function ()
+            @match s begin
+                ::Kind{A, B} => Kind === S && A === Int && B === String
+            end
+        end |> it -> @test it()
     end
+
 end
