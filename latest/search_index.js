a83bd3ca96ef33913810c9cc6829e1e7d85e50dd
autodocs <autodocs>
2018-08-28 05:38:16 +0000

    build based on c5af2f1

--- latest/search_index.js
+++ latest/search_index.js
@@ -181,7 +181,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Pattern function",
     "title": "Pattern function",
     "category": "section",
-    "text": "\n@def f begin\n    # patterns here\n    x                  => 1\n    (x, (1, 2)){x > 3} => 5\n    (x, y)             => 2\n    ::String           => \"is string\"\n    _                  => \"is any\"\nend\nf(1) # => 1\nf(4, (1, 2)) # => 5\nf(1, (1, 2)) # => 2\nf(\"\") # => \"is string\""
+    "text": "Pattern function is a convenient way to define a function with multiple entries.@def f begin\n    # patterns here\n    x                  => 1\n    (x, (1, 2)){x > 3} => 5\n    (x, y)             => 2\n    ::String           => \"is string\"\n    _                  => \"is any\"\nend\nf(1) # => 1\nf(4, (1, 2)) # => 5\nf(1, (1, 2)) # => 2\nf(\"\") # => \"is string\"Take care that until now performance of pattern functions could be not that efficient.The use case of pattern functions is somewherewithout a signaficant requirerment of execution efficiency.\nfilled with tedious business logics.There is promise that we will make outstanding optimizations on pattern functions in the future, where we bootstrap a new MLStyle with the current version. Reservedly, the user interfaces of MLStyle builtin patterns and pattern functions will not change at all, the only one without complete backward compatibility is the way to extending custom patterns, that is to say, if you don\'t use PatternDef.Meta or PatternDef.App, your codes will works well no matter which version of Julia/MLStyle you\'re getting along with."
 },
 
 ]}
