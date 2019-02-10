048d3be278e847e5c08ed43ae613f971a4679c6e
autodocs <autodocs>
2019-02-10 12:19:41 +0000

    build based on 0b2ecc5

--- latest/search_index.js
+++ latest/search_index.js
@@ -405,7 +405,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Static Capturing",
     "title": "Static Capturing",
     "category": "section",
-    "text": "We know that MacroTools.jl has brought about a useful macro @capture to capture specific structures from a given AST.As the motivation of some contributors, @capture of MacroTools.jl has 3 following shortages.Use underscore to denote the structures to be captured, likestruct typename_ field__ end, which makes you have to manually number the captured variables and not that readable or consistent.Cause Side-Effect. The captured variables are entered in current scope.\nLack functionalities like conditional capturing.We can implement several new @capture via MLStyle.jl to get better in all aspects."
+    "text": "We know that MacroTools.jl has brought about a useful macro @capture to capture specific structures from a given AST.As the motivation of some contributors, @capture of MacroTools.jl has 3 following shortages.Use underscore to denote the structures to be captured, like struct typename_ field__ end, which makes you have to manually number the captured variables and not that readable or consistent.\nCause Side-Effect. The captured variables are entered in current scope.\nLack functionalities like conditional capturing.We can implement several new @capture via MLStyle.jl to get better in all aspects."
 },
 
 {
