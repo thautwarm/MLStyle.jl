18ba07d4f1dfea218059d1aac9c95418b72b1e22
thautawarm <twshere@outlook.com>
2020-05-07 23:57:16 +0900

    build-docs scripts

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,13 @@
-cd docs && bash make.sh
-CUR=git branch | grep "*" | cut -c 2-
-VER=python docs/version.py
+cd docs
+bash make.sh
+CUR="`git branch | grep \"*\" | cut -c 2-`"
+echo $CUR
+VER="`python version.py`"
+echo $VER
+cd ../
 git checkout gh-pages
+rm -rf ./$VER/
+mv -T ./sphinx-docs ./$VER/
 git add -A
 git commit -m "docs update, version $VER"
 git push origin gh-pages
