8e3b3c6e33e17c8c7b8ae8cad7ea0bb792506515
thautwarm <twshere@outlook.com>
2020-08-05 14:30:26 +0900

    fix for julia v1.5: enum creation

--- src/DataType.jl
+++ src/DataType.jl
@@ -205,7 +205,7 @@ function impl!(
                                 return $MLStyle.AbstractPatterns.literal($case)
                             ) || error($err_msg)),
                         :($MLStyle.is_enum(::$ctor_name) = true),
-                        :(const $case = $reinterpret($ctor_name, nothing)),
+                        :(const $case = $ctor_name.instance),
                         :($ctor_name() = $case),
                         :($Base.show(io::$IO, ::$ctor_name) = $Base.print(io, $(string(case))))
                     )
